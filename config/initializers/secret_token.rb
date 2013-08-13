139f998224a4143beb86c736d1b56d400f362109
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-13 15:26:35 -0700

    config token / config env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+
 NestedMongoid::Application.config.secret_key_base = '80af31b8815b112ee11407653dd47147840679f2c3dff14f02d3c2662f726ef7f1a6eaf63a1ffc27473205b314f9560b90dc5ceca2c6e8100d3ddaca63450207'
