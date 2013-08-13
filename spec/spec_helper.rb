a2042428f09d40b0bde3a443a85757fb9de3f6aa
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-13 14:38:07 -0700

    clean more spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,18 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'database_cleaner'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
+  config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
 
   config.infer_base_class_for_anonymous_controllers = false
 
